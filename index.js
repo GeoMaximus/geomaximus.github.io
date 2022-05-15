194a567a01de2e0da1a0c5435a88b4d8139a3bcd
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-05-15 21:53:54 +0300

    finally working the way i want it to
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const main = document.getElementsByClassName('main')[0];
 const modal = document.getElementById('modal');
 const addBtn = document.getElementById('button_add');
 const cancelBtn = document.getElementsByClassName('btn')[0];
@@ -15,5 +16,81 @@ window.onclick = function(e) {
   }
 }
 
+fetchArticles();
 
+//CRUD AND FETCH
+async function fetchArticles() {
+  const res = await fetch('http://localhost:3000/articles');
+  if(!res.ok) {
+    const message = `An error occures: ${res.status}`;
+    throw new Error(message);
+  }
+
+  const articlePromise = await res.json().then((res) => {
+    renderArticles(res);
+  });
+}
+
+function renderArticles(articles) {
+  console.log(articles);
+  articles.forEach(article => {
+    createArticle(article);
+  })
+}
+
+function createArticle(article) {
+  const articleHTML = document.createElement('article');
+  const title = document.createElement('h1');
+  title.textContent = article.title;
+  const ul = document.createElement('ul');
+  const liTag = document.createElement('li');
+  liTag.textContent = article.tag;
+  const spanText = document.createElement('span');
+  spanText.textContent = "Added by ";
+  const liAuthor = document.createElement('li');
+  liAuthor.textContent = article.author;
+  const liDate = document.createElement('li');
+  liDate.textContent = article.date;
+
+  ul.appendChild(liTag);
+  liAuthor.appendChild(spanText);
+  ul.appendChild(liAuthor);
+  ul.appendChild(liDate);
+
+  const btnDiv = document.createElement('div');
+  btnDiv.className = 'action_buttons';
+
+  const editBtn = document.createElement('button');
+  editBtn.className = 'action_btn';
+  editBtn.textContent = 'EDIT';
+  editBtn.addEventListener('click', () => {
+    editArticle(article);
+  });
+
+
+  const deleteBtn = document.createElement('button');
+  editBtn.className = 'action_btn';
+  editBtn.textContent = 'DELETE';
+deleteBtn.addEventListener('click', () => {
+  deleteArticle(article);
+});
+
+btnDiv.appendChild(editBtn);
+btnDiv.appendChild(deleteBtn);
+
+const img = document.createElement('img');
+img.src = article.imgUrl;
+
+const content = document.createElement('p');
+content.textContent = article.summary;
+
+articleHTML.appendChild(title);
+articleHTML.appendChild(ul);
+articleHTML.appendChild(btnDiv);
+articleHTML.appendChild(img);
+articleHTML.appendChild(content);
+
+main.appendChild(articleHTML);
+
+}
 
