fb947f659c9d6f6d4f4db019af3c04fa6fa13a6c
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-05-16 23:18:55 +0300

    added next and previous article buttons
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

--- article-details.js
+++ article-details.js
@@ -26,250 +26,265 @@ closeBtn.addEventListener('click', closeModal);
 
 //FETCH ARTICLES FROM SERVER
 async function getArticleFromServer() {
-    const response = await fetch('http://localhost:3000/articles/' + id, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json"
-        }
-    });
-
-    if (!response.ok) {
-        const message = `An error has occured: ${response.status}`;
-        throw new Error(message);
+  const response = await fetch('http://localhost:3000/articles/' + id, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json"
     }
+  });
 
-    await response.json().then((response) => {
-        renderArticle(response);
-    });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  await response.json().then((response) => {
+    renderArticle(response);
+  });
 };
 
 //POST article to server
 async function addArticleToServer() {
-    const article = {
-      title: inputTitle.value,
-      tag: inputTag.value,
-      author: inputAuthor.value,
-      date: inputDate.value,
-      imgUrl: inputImgUrl.value,
-      content: inputContent.value,
-      summary: computeSummary(inputContent.value)
-    }
-  
-    const response = await fetch('http://localhost:3000/articles', {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify(article)
-    });
-  
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      throw new Error(message);
-    }
-  
-    await response.json().then(() => {
-      getArticlesFromServer();
-      resetForm();
-      closeModal();
-    });
+  const article = {
+    title: inputTitle.value,
+    tag: inputTag.value,
+    author: inputAuthor.value,
+    date: inputDate.value,
+    imgUrl: inputImgUrl.value,
+    content: inputContent.value,
+    summary: computeSummary(inputContent.value)
   }
-  
-  //DELETE ARTICLE FROM SERVER
-  async function deleteArticleFromServer(id) {
-    const response = await fetch('http://localhost:3000/articles/' + id, {
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json"
-      },
-    });
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      throw new Error(message);
-    }
-  
-    await response.json().then(() => {
-      getArticlesFromServer();
-    });
+
+  const response = await fetch('http://localhost:3000/articles', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(article)
+  });
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
   }
-  
-  //UPDATE ARTICLE FROM SERVER
-  async function updateArticleToServer(id) {
-    const article = {
-      title: inputTitle.value,
-      tag: inputTag.value,
-      author: inputAuthor.value,
-      date: inputDate.value,
-      imgUrl: inputImgUrl.value,
-      content: inputContent.value,
-      summary: computeSummary(inputContent.value)
-    }
-  
-    const response = await fetch('http://localhost:3000/articles/' + id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(article)
-    });
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      throw new Error(message);
-    }
-    await response.json().then(() => {
-      getArticlesFromServer();
-      resetForm();
-      closeModal();
-    });
+
+  await response.json().then(() => {
+    getArticlesFromServer();
+    resetForm();
+    closeModal();
+  });
+}
+
+//DELETE ARTICLE FROM SERVER
+async function deleteArticleFromServer(id) {
+  const response = await fetch('http://localhost:3000/articles/' + id, {
+    method: 'DELETE',
+    headers: {
+      "Content-Type": "application/json"
+    },
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
   }
-  
-  function openAddModal() {
-    clearSaveButtonEvents();
-    saveBtn.addEventListener('click', function () {
-      addArticleToServer()
-    });
-  
-    body.className = 'show-modal';
+
+  await response.json().then(() => {
+    getArticlesFromServer();
+  });
+}
+
+//UPDATE ARTICLE FROM SERVER
+async function updateArticleToServer(id) {
+  const article = {
+    title: inputTitle.value,
+    tag: inputTag.value,
+    author: inputAuthor.value,
+    date: inputDate.value,
+    imgUrl: inputImgUrl.value,
+    content: inputContent.value,
+    summary: computeSummary(inputContent.value)
   }
-  
-  function openEditModal(article) {
-    inputTitle.value = article.title;
-    inputTag.value = article.tag;
-    inputAuthor.value = article.author;
-    inputDate.value = article.date;
-    inputImgUrl.value = article.imgUrl;
-    inputContent.value = article.content;
-  
-    clearSaveButtonEvents();
-  
-    saveBtn.addEventListener('click', function () {
-      updateArticleToServer(article.id);
-    });
-  
-    body.className = 'show-modal';
+
+  const response = await fetch('http://localhost:3000/articles/' + id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(article)
+  });
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
   }
-  
-  
+  await response.json().then(() => {
+    getArticlesFromServer();
+    resetForm();
+    closeModal();
+  });
+}
+
+function openAddModal() {
+  clearSaveButtonEvents();
+  saveBtn.addEventListener('click', function () {
+    addArticleToServer()
+  });
+
+  body.className = 'show-modal';
+}
+
+function openEditModal(article) {
+  inputTitle.value = article.title;
+  inputTag.value = article.tag;
+  inputAuthor.value = article.author;
+  inputDate.value = article.date;
+  inputImgUrl.value = article.imgUrl;
+  inputContent.value = article.content;
+
+  clearSaveButtonEvents();
+
+  saveBtn.addEventListener('click', function () {
+    updateArticleToServer(article.id);
+  });
+
+  body.className = 'show-modal';
+}
+
+
 
 function removeOldArticles() {
-    while (main.firstChild) {
-      main.removeChild(main.firstChild);
-    }
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
   }
+}
 
 function createArticle(article) {
-    //create article node
-    const articleHTML = document.createElement('article');
-    articleHTML.className = 'article-container';
+  //create article node
+  const articleHTML = document.createElement('article');
+  articleHTML.className = 'article-container';
 
-    //create title
-    const title = document.createElement('h1');
-    title.textContent = article.title;
+  //create title
+  const title = document.createElement('h1');
+  title.textContent = article.title;
 
-    //create ul element for info
-    const ul = document.createElement('ul');
-    ul.className = 'info';
+  //create ul element for info
+  const ul = document.createElement('ul');
+  ul.className = 'info';
 
-    //create tag
-    const liTag = document.createElement('li');
-    liTag.className = 'info_item';
-    liTag.textContent = article.tag;
+  //create tag
+  const liTag = document.createElement('li');
+  liTag.className = 'info_item';
+  liTag.textContent = article.tag;
 
-    //create added by
-    const liText = document.createElement('li');
-    liText.className = 'info_item';
-    liText.textContent = "Added by ";
+  //create added by
+  const liText = document.createElement('li');
+  liText.className = 'info_item';
+  liText.textContent = "Added by ";
 
-    //create author
-    const spanAuthor = document.createElement('span');
-    spanAuthor.textContent = article.author;
-    spanAuthor.className = 'info_mark';
+  //create author
+  const spanAuthor = document.createElement('span');
+  spanAuthor.textContent = article.author;
+  spanAuthor.className = 'info_mark';
 
-    //create date
-    const liDate = document.createElement('li');
-    liDate.textContent = article.date;
-    liDate.className = 'info_item';
+  //create date
+  const liDate = document.createElement('li');
+  liDate.textContent = article.date;
+  liDate.className = 'info_item';
 
-    liText.appendChild(spanAuthor);
-    ul.appendChild(liTag);
-    ul.appendChild(liText);
-    ul.appendChild(liDate);
+  liText.appendChild(spanAuthor);
+  ul.appendChild(liTag);
+  ul.appendChild(liText);
+  ul.appendChild(liDate);
 
-    //div for buttons
-    const btnDiv = document.createElement('div');
-    btnDiv.className = 'action_buttons';
+  //div for buttons
+  const btnDiv = document.createElement('div');
+  btnDiv.className = 'action_buttons';
 
 
-    //edit button
-    const editBtn = document.createElement('button');
-    editBtn.className = 'action_btn';
-    editBtn.textContent = 'EDIT';
-    editBtn.addEventListener('click', function () {
-        openEditModal(article);
-    });
+  //edit button
+  const editBtn = document.createElement('button');
+  editBtn.className = 'action_btn';
+  editBtn.textContent = 'EDIT';
+  editBtn.addEventListener('click', function () {
+    openEditModal(article);
+  });
 
-    //delete button
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'action_btn';
-    deleteBtn.textContent = 'DELETE';
-    deleteBtn.addEventListener('click', function () {
-        deleteArticleFromServer(article.id);
-        window.location.replace("/index.html");
-    });
+  //delete button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'action_btn';
+  deleteBtn.textContent = 'DELETE';
+  deleteBtn.addEventListener('click', function () {
+    deleteArticleFromServer(article.id);
+    window.location.replace("/index.html");
+  });
 
-    btnDiv.appendChild(editBtn);
-    btnDiv.appendChild(deleteBtn);
+  btnDiv.appendChild(editBtn);
+  btnDiv.appendChild(deleteBtn);
 
-    //add image
-    const img = document.createElement('img');
-    img.setAttribute('src', article.imgUrl);
+  //add image
+  const img = document.createElement('img');
+  img.setAttribute('src', article.imgUrl);
 
-    //add paragraph
-    const contentDiv = document.createElement('div');
-    contentDiv.className = 'content';
-    const content = document.createElement('p');
-    content.className = 'article-container';
-    content.textContent = article.content;
-    contentDiv.appendChild(content);
+  //add paragraph
+  const contentDiv = document.createElement('div');
+  contentDiv.className = 'content';
+  const content = document.createElement('p');
+  content.className = 'article-container';
+  content.textContent = article.content;
+  contentDiv.appendChild(content);
 
-    articleHTML.appendChild(title);
-    articleHTML.appendChild(ul);
-    articleHTML.appendChild(btnDiv);
-    articleHTML.appendChild(img);
-    articleHTML.appendChild(contentDiv);
+  articleHTML.appendChild(title);
+  articleHTML.appendChild(ul);
+  articleHTML.appendChild(btnDiv);
+  articleHTML.appendChild(img);
+  articleHTML.appendChild(contentDiv);
 
-    return articleHTML;
+  return articleHTML;
 
 }
 
 function renderArticle(article) {
-    removeOldArticles();
-    let articleNode = createArticle(article);
-    main.appendChild(articleNode);
+  removeOldArticles();
+  let articleNode = createArticle(article);
+  main.appendChild(articleNode);
 }
 
 function resetForm() {
-    inputTitle.value = "";
-    inputTag.value = "";
-    inputAuthor.value = "";
-    inputDate.value = "";
-    inputImgUrl.value = "";
-    inputContent.value = "";
-  }
-  
-  function clearSaveButtonEvents() {
-    let newUpdateButton = saveBtn.cloneNode(true);
-    saveBtn.parentNode.replaceChild(newUpdateButton, saveBtn);
-    saveBtn = document.getElementById('save');
-  }
-  
-  function closeModal() {
-    body.className = '';
-  }
+  inputTitle.value = "";
+  inputTag.value = "";
+  inputAuthor.value = "";
+  inputDate.value = "";
+  inputImgUrl.value = "";
+  inputContent.value = "";
+}
 
-  function computeSummary(content) {
-    return content.substring(0, 1000);
-  }
-  
-  
+function clearSaveButtonEvents() {
+  let newUpdateButton = saveBtn.cloneNode(true);
+  saveBtn.parentNode.replaceChild(newUpdateButton, saveBtn);
+  saveBtn = document.getElementById('save');
+}
+
+function closeModal() {
+  body.className = '';
+}
+
+function computeSummary(content) {
+  return content.substring(0, 1000);
+}
+
+const prevArticle = document.getElementById('prev-article');
+const nextArticle = document.getElementById('next-article');
+
+prevArticle.addEventListener('click', redirectToPrev);
+nextArticle.addEventListener('click', redirectToNext);
+
+function redirectToPrev() {
+  window.location.replace("/article-details.html/?id=" + (id - 1));
+  getArticleFromServer();
+}
+
+function redirectToNext() {
+  window.location.replace("/article-details.html/?id=" + (parseInt(id) + 1));
+  getArticleFromServer();
+}
+
 getArticleFromServer();
